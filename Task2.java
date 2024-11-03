49d6ec5917fb238ac2e97c2424cb2991c552eefd
yaroslavstepanov <yaroslavstepanov2018@gmail.com>
2024-11-03 14:16:31 +0300

    Create Task2.java

---
+++ Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+    public static void main(String[] args) {
+        double numDouble = 13.1234567999D;
+        byte numByte = Byte.MAX_VALUE;
+        System.out.println(numDouble);
+        System.out.println(numByte);
+    }
+}
