830d6b63df48e6f963f16816c609cb8669981dc2
yaroslavstepanov <yaroslavstepanov2018@gmail.com>
2024-11-09 00:16:05 +0300

    Revert "Merge branch 'main' of https://github.com/yaroslavstepanov/JavaBase"
    This reverts commit bc6310a77eab5391c258003e13e086fea94903f3, reversing
    changes made to d0f78b05b4d94714b1b049962367081f0ce2fb1f.

--- Task8.java
+++ Task8.java
@@ -4,11 +4,11 @@ public class Task8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Запрос числа у пользователя с проверкой
+        // Запрос числа у пользователя с проверкой на корректность ввода
         System.out.print("Введите любое целое положительное число: ");
         while (!scanner.hasNextInt()) {
             System.out.println("Ошибка: введите корректное целое число!");
-            scanner.next(); // Очистка
+            scanner.next(); // Очистка некорректного ввода
         }
         int n = scanner.nextInt();
 
@@ -23,6 +23,7 @@ public class Task8 {
                 sum += i;
             }
 
+            // Вывод результата
             System.out.println("Сумма всех нечетных чисел от 1 до " + n + " равна: " + sum);
         }
 
