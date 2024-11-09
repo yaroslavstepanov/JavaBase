f0ffcaa01e1850b09299ee773b9b4e8ef3e4a5a5
yaroslavstepanov <yaroslavstepanov2018@gmail.com>
2024-11-09 13:23:48 +0300

    Create Task8.java

--- Task8.java
+++ Task8.java
@@ -4,11 +4,11 @@ public class Task8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Запрос числа у пользователя с проверкой на корректность ввода
+        // Запрос числа у пользователя с проверкой
         System.out.print("Введите любое целое положительное число: ");
         while (!scanner.hasNextInt()) {
             System.out.println("Ошибка: введите корректное целое число!");
-            scanner.next(); // Очистка некорректного ввода
+            scanner.next(); // Очистка
         }
         int n = scanner.nextInt();
 
@@ -22,8 +22,7 @@ public class Task8 {
             for (int i = 1; i <= n; i += 2) {
                 sum += i;
             }
-
-            // Вывод результата
+            
             System.out.println("Сумма всех нечетных чисел от 1 до " + n + " равна: " + sum);
         }
 
